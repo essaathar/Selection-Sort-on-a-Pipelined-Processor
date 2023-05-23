9a638b8acd134bc73cf1fc89b32d2b3f76f4cb98
essaathar <122386200+essaathar@users.noreply.github.com>
2023-05-23 18:23:20 +0500

    Python code for reference
    Code taken from https://www.programiz.com/dsa/bubble-sort#:~:text=Bubble%20sort%20is%20a%20sorting,is%20called%20a%20bubble%20sort.

--- selection_sort.py
+++
@@ -1,8 +0,0 @@
-#Sorting Algorithm: Selection Sort
-
-def selectionsort(lst):
-   for i in range(len(lst)):
-      for j in range(i,len(lst)):
-          if lst[j] < lst[i]:
-              lst[i], lst[j] = lst[j], lst[i]
-   return lst
